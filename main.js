b2988839731296434ace2cc5ecedb2b702c71acc
Joaquin Hernandez Martinez <proyecto_noether@outlook.com>
2023-08-12 19:32:00 +0200

    fixed localhost

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     mi_fetch.addEventListener('click', async () => {
         try {
-            const url = 'http://0.0.0.0:8081/date'
+            const url = 'http://127.0.0.1:8081/date'
             const response = await fetch(url);  // Coge de forma async la respuesta
             const data = await response.json();  //Evalua de form a async el cuerpo (payload)
             broma.innerHTML = `<p>${data.date}</p>`;
