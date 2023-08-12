9c3387b65264b28047f32c3fb8d8dd9da53b9fcf
Joaquin Hernandez Martinez <proyecto_noether@outlook.com>
2023-08-12 19:25:39 +0200

    trying new branch

--- main.js
+++ main.js
@@ -4,11 +4,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     mi_fetch.addEventListener('click', async () => {
         try {
-            //const url = '0.0.0.0:8081'
-            const url = 'https://api.chucknorris.io/jokes/random'
+            const url = 'http://0.0.0.0:8081'
             const response = await fetch(url);  // Coge de forma async la respuesta
             const data = await response.json();  //Evalua de form a async el cuerpo (payload)
-            broma.innerHTML = `<p>${data.value}</p>`;
+            broma.innerHTML = `<p>${data.date}</p>`;
         } catch (error) {
             console.error('Error fetching API:', error);
             broma.innerHTML = '<p>No se han conseguido los datos</p>';
